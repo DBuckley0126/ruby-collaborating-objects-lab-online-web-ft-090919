c7744aac5f33ad02433597ae9c24bddaa7cbe546
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-26 23:53:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,24 @@
 class Song
 
+@@all = []
+
+def self.all
+  @@all
+end
+
+
 attr_accessor :name
+attr_reader :artist
 
 def initialize (name)
   @name = name
+  @@all < self
 end
 
 def artist= (artist_obj)
   @artist = artist_obj
 end
+
+
+
 end
