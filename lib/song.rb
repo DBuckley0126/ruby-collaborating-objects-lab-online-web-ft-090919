d0757c05928b774f6cff0cf289d83e725687b581
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-27 01:04:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ def self.all
 end
 
 def self.new_by_filename (file)
-  artist, name = file.split(" - ")
+  name, artist = file.split(" - ")
   var = self.new(name)
   var.artist = Artist.find_or_create_by_name(artist)
 end
