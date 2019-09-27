9723c8753530793a9ff4d9e395af1052a369ddfd
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-27 00:57:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,6 +3,7 @@ require 'pry'
 class MP3Importer
 
 attr_accessor :path, :files
+
   def initialize (path)
     @path = path
     @files = Dir.entries(path).select {|f| !File.directory? f}
@@ -10,7 +11,9 @@ attr_accessor :path, :files
   end
 
   def import
-
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
   end
 
 
