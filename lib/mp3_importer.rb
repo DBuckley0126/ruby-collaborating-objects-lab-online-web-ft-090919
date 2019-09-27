37dea2715fb6a9b53a9dd5baff622cf16e484a26
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-27 00:36:30 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,7 @@ class MP3Importer
 attr_accessor :path, :files
   def initialize (path)
     @path = path
-    @files = Dir.entries(path)
+    @files = Dir.entries(path).select {|f| self.file? f}
     binding.pry
   end
 
